c3fffe6d3d5a30481c27a54505566893cef2de97
minh1996bk <minh1996bk@gmail.com>
2018-01-28 10:10:11 +0700

    add view for streamer channel

--- WebChatApplication.js
+++ WebChatApplication.js
@@ -1,13 +1,32 @@
+
+// configure web app
 var express = require('express');
 var app = express();
 app.use(express.static('./public'));
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
-
-var server = require('http').Server(app);
-server.listen(3000);
-
 app.get('/home', function(req, res) {
     res.render('home');
+});
+
+app.get('/streamer', function(req, res) {
+    res.render('StreamerChannel');
+});
+
+
+// launching app to server
+var server = require('http').Server(app);
+
+server.listen(3000);
+
+
+// create io that listen to server 
+var io = require('socket.io')(server);
+
+io.on('connection', function(socket) {
+    console.log("User connected: " + socket.id);
+    socket.on('disconnect', function() {
+        console.log(socket.id + ' disconnected!');
+    });
 });
